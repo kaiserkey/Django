21218a5d91ffa1c42a1e2c19712cfd7749b84471
kaiserkey <kaiserkey2@gmail.com>
2023-03-10 00:11:36 -0300

    new changes

--- first_proyect/myfirstweb/myfirstapp/admin.py
+++ first_proyect/myfirstweb/myfirstapp/admin.py
@@ -4,7 +4,14 @@ from django.contrib import admin
 from .models import Post
 
 
-class Pos
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish', 'status')
+    list_filter = ('status', 'created_date', 'publish', 'author')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
 
 
 # Register your models here.
