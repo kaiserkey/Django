b5fdb9b9afe8dfc17776cc74605c365f5a85ee1b
kaiserkey <kaiserkey2@gmail.com>
2023-03-09 23:22:22 -0300

    new changes

--- first_proyect/myfirstweb/myfirstapp/models.py
+++ first_proyect/myfirstweb/myfirstapp/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS, default='draft')
     
+    
     def __str__(self):
         return self.title
     
