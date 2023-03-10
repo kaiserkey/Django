c49e0c34c13002399d9bca8f97a96f4122c0376a
kaiserkey <kaiserkey2@gmail.com>
2023-03-10 00:14:54 -0300

    new changes

--- first_proyect/myfirstweb/myfirstapp/admin.py
+++ first_proyect/myfirstweb/myfirstapp/admin.py
@@ -8,7 +8,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publish', 'status')
     list_filter = ('status', 'created_date', 'publish', 'author')
     search_fields = ('title', 'content')
-    prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
