bc8aacba847fd26d9a9f49e5ea300dc4d29c1150
kaiserkey <kaiserkey2@gmail.com>
2023-03-11 00:31:39 -0300

    new changes

--- first_proyect/myfirstweb/myfirstapp/models.py
+++ first_proyect/myfirstweb/myfirstapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from 
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 @python_2_unicode_compatible
 class Post(models.Model):
