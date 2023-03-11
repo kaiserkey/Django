b338ade4899913c0770b2e0acd9446bb4444e860
kaiserkey <kaiserkey2@gmail.com>
2023-03-11 01:51:51 -0300

    new changes

--- first_proyect/myfirstweb/myfirstapp/views.py
+++ first_proyect/myfirstweb/myfirstapp/views.py
@@ -4,4 +4,4 @@ from myfirstapp.models import Post
 # Create your views here.
 def home(request):
     return render(request, 'post/index.html',
-                    {'posts': Post.objects.all()})
+                    {'posts': Post.objects.count()})
