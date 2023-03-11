3419b89c13beaec87930e5762a2d59cfe68dd756
kaiserkey <kaiserkey2@gmail.com>
2023-03-11 01:47:11 -0300

    new changes

--- first_proyect/myfirstweb/myfirstapp/views.py
+++ first_proyect/myfirstweb/myfirstapp/views.py
@@ -4,4 +4,4 @@ from myfirstapp.models import Post
 # Create your views here.
 def home(request):
     return render(request, 'post/index.html',
-                    {'title': Post.title(), 'content': 'Welcome to the home page!'})
+                    {'title': Post.title(), 'content': Post.content()})
