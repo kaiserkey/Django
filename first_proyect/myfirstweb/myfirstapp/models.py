edb7c28364aa82eadbc1838dfc1717caac741cc4
kaiserkey <kaiserkey2@gmail.com>
2023-03-11 00:34:52 -0300

    new changes

--- first_proyect/myfirstweb/myfirstapp/models.py
+++ first_proyect/myfirstweb/myfirstapp/models.py
@@ -2,9 +2,9 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+from django.utils.encoding import python_4_unicode_compatible
 # Create your models here.
-@python_2_unicode_compatible
+@python_4_unicode_compatible
 class Post(models.Model):
     STATUS = (  ('draft','Draft'),
                 ('published','Published'))
