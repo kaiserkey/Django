19ddc397145ea1a2c9f2dbd89d615a4f29c0e943
kaiserkey <kaiserkey2@gmail.com>
2023-03-11 22:31:18 -0300

    new changes

--- django_second_project/myapp/views.py
+++ django_second_project/myapp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return HttpResponse("<h1 color=">Hello World</h1>")
 
