abf0a141d270a8a22b221de40bde1f7a537fc4f5
kaiserkey <kaiserkey2@gmail.com>
2023-03-12 14:30:37 -0300

    new changes

--- django_second_project/myapp/urls.py
+++ django_second_project/myapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.hello),
     path('about/', views.about),
     path('datos/<str:username>', views.datos),
+    path('project/', views.project),
     path('task/', views.task),
-    path('project/', views.project)
 ]
