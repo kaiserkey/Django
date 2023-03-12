1df0efe7d39e75a6c1bb1799b0c311358754020c
kaiserkey <kaiserkey2@gmail.com>
2023-03-12 18:57:32 -0300

    new changes

--- django_second_project/myapp/models.py
+++ django_second_project/myapp/models.py
@@ -11,3 +11,6 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
