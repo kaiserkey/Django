122b61072efac668a47a5ef26bac34e94ab5bbfa
kaiserkey <kaiserkey2@gmail.com>
2023-03-12 16:09:00 -0300

    new changes

--- django_second_project/myapp/views.py
+++ django_second_project/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
+from django.shortcuts import rende
 
 # Create your views here.
 def hello(request):
