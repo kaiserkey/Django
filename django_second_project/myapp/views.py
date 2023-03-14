4c3ad64a9022b83103fc822d4713d1b9a35ba12c
kaiserkey <kaiserkey2@gmail.com>
2023-03-14 01:05:19 -0300

    new changes

--- django_second_project/myapp/views.py
+++ django_second_project/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
 from django.shortcuts import get_object_or_404
-fr
+from .forms import CreateNewTask
 
 # Create your views here.
 def index(request):
