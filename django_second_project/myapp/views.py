998ae7513466668731f74e5e825ceaa9fb69555c
kaiserkey <kaiserkey2@gmail.com>
2023-03-13 22:25:01 -0300

    new changes

--- django_second_project/myapp/views.py
+++ django_second_project/myapp/views.py
@@ -8,7 +8,7 @@ def index(request):
     return render(request, "index.html")
 
 def about(request):
-    return render(request, "about.html"")
+    return render(request, "about.html")
 
 def datos(request, username):
     print(username)
