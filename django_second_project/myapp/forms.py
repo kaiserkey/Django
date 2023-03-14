f9a97c05a09718440ba71dc0d10eebdddfca4c43
kaiserkey <kaiserkey2@gmail.com>
2023-03-14 01:02:55 -0300

    new changes

--- django_second_project/myapp/forms.py
+++ django_second_project/myapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class CreateNewTask():
     title = forms.CharField(label="Title", max_length=200)
-    description =.CharField(max_length=length, ${blank=True, null=True})
+    description = forms.CharField()
