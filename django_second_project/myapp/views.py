bd310cd473af5b5bea25b52be1a24bac783836d1
kaiserkey <kaiserkey2@gmail.com>
2023-03-14 01:04:49 -0300

    new changes

--- django_second_project/myapp/views.py
+++ django_second_project/myapp/views.py
@@ -26,4 +26,4 @@ def task(request):
     return render(request, "task.html", {"task": tasks})
 
 def newTask(request):
-    return render(request, "newTask.html", {"fo"})
+    return render(request, "newTask.html", {"form": form})
