ba865d93c84d37534afc46d77bc4b1075f4aee80
kaiserkey <kaiserkey2@gmail.com>
2023-03-14 01:01:49 -0300

    new changes

--- django_second_project/myapp/forms.py
+++ django_second_project/myapp/forms.py
@@ -1,1 +1,7 @@
 from django import forms
+
+class CreateNewTask():
+    title = forms.CharField(label="Title", max_length=200)
+    content = forms.CharField(label="Content", max_length=200)
+    due_date = forms.DateField(label="Due date")
+    priority = forms.CharField(label="Priority", max_length=200)
