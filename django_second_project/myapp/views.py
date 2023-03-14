1173ad9a071087cde2c147500618013be887f1e6
kaiserkey <kaiserkey2@gmail.com>
2023-03-13 22:28:00 -0300

    new changes

--- django_second_project/myapp/views.py
+++ django_second_project/myapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
-    title = 
+    title = "Welcome to the "
     return render(request, "index.html")
 
 def about(request):
