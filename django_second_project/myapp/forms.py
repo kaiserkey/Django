f75e3bd913e0fbacc6b5da37648365b88ec9b8e5
kaiserkey <kaiserkey2@gmail.com>
2023-03-14 01:09:33 -0300

    new changes

--- django_second_project/myapp/forms.py
+++ django_second_project/myapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class CreateNewTask(forms.Form):
     title = forms.CharField(label="Titulo de la tarea", max_length=200)
-    description = forms.Textarea()
+    description = forms.CharField(w)
