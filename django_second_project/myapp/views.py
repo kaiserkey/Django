232fc891e89fc3da227c7fd2ab7eacee8baed3b3
kaiserkey <kaiserkey2@gmail.com>
2023-03-14 23:35:33 -0300

    new changes

--- django_second_project/myapp/views.py
+++ django_second_project/myapp/views.py
@@ -19,12 +19,12 @@ def datos(request, username):
 
 def project(request):
     projects = Project.objects.all()
-    return render(request, "project.html", {"projects": projects})
+    return render(request, "tasks/project.html", {"projects": projects})
 
 def task(request):
     tasks = Task.objects.all()
     #task = get_object_or_404(Task, id=id)
-    return render(request, "task.html", {"task": tasks})
+    return render(request, "tasks/task.html", {"task": tasks})
 
 def newTask(request):
     if request.method == "POST":
